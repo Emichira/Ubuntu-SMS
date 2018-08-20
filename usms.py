c9bf46ed5729ce4115a0b3961cc937c2a6ab1e0b
michira <emmanuelmichira@gmail.com>
2018-08-20 16:04:52 +0300

    Add js and css link to layout file

--- usms.py
+++ usms.py
@@ -2,25 +2,25 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/index")
+@app.route("/home")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', title=Home)
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    return render_template('login.html', title=Login)
 
 @app.route("/register")
 def register():
-    return render_template('register.html')
+    return render_template('register.html', title=Register)
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', title=Contact)
 
 @app.route("/error")
 def notFound():
-    return render_template('404.html')
+    return render_template('404.html', title=404)
     
 
 if __name__ == '__main__':
