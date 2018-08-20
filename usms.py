bf44e2d61ee2218a3a5607eff6c5f84fad4b183f
michira <emmanuelmichira@gmail.com>
2018-08-20 12:15:22 +0300

    Add html templates Add routes to the templates

--- usms.py
+++ usms.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_temlate
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "<h1>Hello World!</h1>"
+def home():
+    return render_temlate('index.html')
+
+@app.route("/")
+def login():
+    return render_temlate('login.html')
+
+@app.route("/")
+def register():
+    return render_temlate('register.html')
+
+@app.route("/")
+def contact():
+    return render_temlate('contact.html')
+
+@app.route("/")
+def notFound():
+    return render_temlate('404.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
