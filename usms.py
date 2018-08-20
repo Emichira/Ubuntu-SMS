6d00258f5a59e0d469965b0c6fc8b7229687d102
michira <emmanuelmichira@gmail.com>
2018-08-20 16:11:53 +0300

    Fixed bugs and syntax errors

--- usms.py
+++ usms.py
@@ -4,23 +4,23 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('index.html', title=Home)
+    return render_template('index.html', title='Home')
 
 @app.route("/login")
 def login():
-    return render_template('login.html', title=Login)
+    return render_template('login.html', title='Login')
 
 @app.route("/register")
 def register():
-    return render_template('register.html', title=Register)
+    return render_template('register.html', title='Register')
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html', title=Contact)
+    return render_template('contact.html', title='Contact')
 
 @app.route("/error")
 def notFound():
-    return render_template('404.html', title=404)
+    return render_template('404.html', title='404')
     
 
 if __name__ == '__main__':
