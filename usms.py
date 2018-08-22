360f356049eb00e909951adf7b9ccbc2e14fd5df
michira <emmanuelmichira@gmail.com>
2018-08-22 11:14:05 +0300

    Add navigation link to dashboard index page in login file Add routes for dashboard pages

--- usms.py
+++ usms.py
@@ -18,10 +18,33 @@ def register():
 def contact():
     return render_template('contact.html', title='Contact')
 
-@app.route("/404")
-def notFound():
-    return render_template('404.html', title='404')
-    
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard/index.html', title='dashboard')
+
+@app.route("/administration")
+def admin():
+    return render_template('administration.html', title='404')
+
+@app.route("/api-documents")
+def api_documents():
+    return render_template('api-documents.html', title='API Documents')
+
+@app.route("/pages-404")
+def dashboardNotFound():
+    return render_template('pages-error-404.html', title='404')
+
+@app.route("/pages-500")
+def serviceError():
+    return render_template('pages-error-500.html', title='Service Error')
+
+@app.route("/pages-profile")
+def profile():
+    return render_template('pages-profile.html', title='User Account')    
+
+@app.route("/smpp")
+def smpp():
+    return render_template('smpp.html', title='User Account')
 
 if __name__ == '__main__':
     app.run(debug=True)
