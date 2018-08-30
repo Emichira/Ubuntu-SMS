e27bc8459d39a4733a626c820ef839183b4dcef8
michira <emmanuelmichira@gmail.com>
2018-08-30 12:19:28 +0300

    Add extension to handle and manage user sesions

--- usms/__init__.py
+++ usms/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 # app initiliazation
 app = Flask(__name__)
@@ -12,5 +13,6 @@ app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 app.debug = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from usms import routes
--- usms/models.py
+++ usms/models.py
@@ -1,6 +1,11 @@
-from usms import db
+from usms import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
 # User class handles registration and login of users
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
