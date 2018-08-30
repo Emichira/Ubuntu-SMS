c31231aadacf6ad593142e6841f1a5c4f183728e
michira <emmanuelmichira@gmail.com>
2018-08-30 16:12:52 +0300

    Modify logout functionality to display logout in place of login and logout when user is logged in

--- usms/routes.py
+++ usms/routes.py
@@ -11,8 +11,8 @@ def home():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('dashboard'))
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -26,8 +26,8 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('dashboard'))
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
