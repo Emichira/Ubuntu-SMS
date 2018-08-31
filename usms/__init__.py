e6d1faedf928fa6aebeecb9d4ca24564d229712f
michira <emmanuelmichira@gmail.com>
2018-08-31 12:08:10 +0300

    Modify Procfile to specify file to run.py as python file to execute Add new application dependencies to enable deployment to heroku Modify __init__.py file edit import and database connections for deployment to heroku

--- usms/__init__.py
+++ usms/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask.ext.heroku import Heroku
 
 # app initiliazation
 app = Flask(__name__)
@@ -16,5 +17,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+heroku = Heroku(app)
+
 
 from usms import routes
