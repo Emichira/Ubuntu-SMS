dc866a4e38581e6b10cf26145e148ab15c5264d1
Emmanuel Michira <emmanuelmichira@gmail.com>
2018-09-10 01:08:46 +0300

    Modify the database name to ubuntu

--- usms/__init__.py
+++ usms/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '18be081e801b18259fc7a92ce165329a'
 
 # initialize our db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Mich$anuel1@localhost/ubuntu_usms'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Mich$anuel1@localhost/ubuntu'
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 app.debug = True
 db = SQLAlchemy(app)
